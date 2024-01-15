a70d3eb21c79f2dff5b9780cf49e4b39aa2ebc7a
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2024-01-15 08:30:50 +0000

    GREEN-52 Adding logout functionality. Tested the API on Postman

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,9 @@
 
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from accounts import views
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
+    path('logout_user/', views.logout_user, name='logout_user'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
+
+
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+@api_view(["POST", ])
+def logout_user(request):
+    if request.method == "POST":
+        request.user.auth_token.delete()
+        return Response({"message": "You are logged out"}, status=status.HTTP_200_OK)
