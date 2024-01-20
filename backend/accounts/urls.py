0a9e82b077f86758c74e3b191a8d4377b3fa152a
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2024-01-20 17:24:25 +0000

    GREEN-66 Restructured the backend code. Tested and is working as expected

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,7 +2,7 @@
 
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from accounts import views
+from . import views
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
