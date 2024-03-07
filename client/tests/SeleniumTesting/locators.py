87f1e1dbfbf5366f7d8a9ad9c9056d8ea9e11772
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2024-03-07 12:32:12 +0000

    GREEN-172 Adding sign in page locators, elements and methods to be used in sign in page

--- client/tests/SeleniumTesting/locators.py
+++ client/tests/SeleniumTesting/locators.py
@@ -1,6 +1,9 @@
 
 from selenium.webdriver.common.by import By
 
+class VARIABLES(object):
+    URL = "https://v2.globalgreeninit.world/"
+
 class HomePageLocators(object):
     SIGNIN = (By.XPATH, "//button[@type='submit']")  # Entering from homepage to signIn page
 
@@ -9,16 +12,32 @@ class SignUpPageLocators:
     EMAIL_CLK = (By.XPATH, "//input[@class='input c2a857a25 c27e670b0']") # Able to set value to Email
     EMAIL_TXT = (By.XPATH, "//div[@class='c2809db4e js-required c6bbe06f6 c5d75975c']")
     EMAIL_ERR = (By.ID, 'error-element-email')
-    PASSWORD_CLK = (By.XPATH, "//input[@class='input c2a857a25 c70ffb6a4']")
+    PASSWORD_CLK = (By.XPATH,    "//input[@class='input c2a857a25 c70ffb6a4']")
     PASSWORD_TXT = (By.XPATH, "//div[@class='c2809db4e js-required c6bbe06f6 c005e1e8d']")
     PASSWORD_ERR = (By.XPATH, "//li[@data-error-code='password-policy-length-at-least']")
     PASSWORD_COMPLEXITY = (By.XPATH, "//div[@class='cf325142a']")
     SIGNUP = (By.XPATH, "//a[@class='cc3100fd0 c316efbcd']")  # Entering from signIn page to singUp page (LOGIN)
     SIGNUP_ERR = (By.XPATH, "//p[@class='ce3cf3592 c409e6d91']")
-    SUBMIT = (By.NAME, "action") # Entering from
+    SUBMIT = (By.NAME, "action")
 
 
 class ContentPageLocators(object):
     CIRCLE_LINK = (By.XPATH, "//div[@class='h-12 w-12 cursor-pointer rounded-full bg-black']")
     LOGGED_EMAIL = (By.XPATH, "//div[@class='grid grid-rows-2']//p")
     LOGOUT_BUTTON = (By.XPATH, "//div[@class='grid grid-rows-2']//button")
+
+class SignInPageLocators:
+    SIGNIN = (By.XPATH, "//button[@type='submit']")
+    EMAIL_CLK = (By.ID, "username")  # Able to set value to Email
+    EMAIL_TXT = (By.XPATH, "//div[@data-dynamic-label-for='username']")
+    PASSWORD_CLK = (By.ID, "password")
+    PASSWORD_TXT = (By.XPATH, "//div[@data-dynamic-label-for='password']")
+    FORGOT_PASSWORD = (By.XPATH, "//a[@class='ce78228eb c80853310 cfe4c492b']")
+    EMAIL_FORGOT_PASSWORD = (By.ID, "email") # Email box on forgot password page
+    FORGOT_PASSWORD_PAGE = (By.XPATH, "//h1[@class='c75a821d8 cf4ccfc47']") # Forgot Password Text on forgot password page
+    USER_EMAIL = "moosasharieff@gmail.com"
+    USER_PASSWORD = "testing@123"
+    INCORRECT_EMAIL_OR_PASSWORD = (By.XPATH, "//span[@id='error-element-password']")
+    CARDS = (By.XPATH, "//img[@class='rounded-xl object-fill']")
+    BACK_TO_LOGIN = (By.XPATH, "//button[@value='back-to-login']")
+    EMAIL_CONFIRMATION = (By.XPATH, "//h1[@role='presentation']")
--- client/tests/SeleniumTesting/pages.py
+++ client/tests/SeleniumTesting/pages.py
@@ -1,19 +1,33 @@
-
+import time
 
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from elements import Email_Field_Elements, Password_Field_Elements
-from locators import HomePageLocators, ContentPageLocators, SignUpPageLocators
+from elements import Email_Field_Elements, Password_Field_Elements, Forgot_Password_Field_Elements
+from locators import HomePageLocators, ContentPageLocators, SignUpPageLocators, SignInPageLocators
 
-class Email_Element(Email_Field_Elements):
+class SignUp_Email_Element(Email_Field_Elements):
     """ This class get the search text from the specified locator """
     forEdit = SignUpPageLocators.EMAIL_CLK
     forRead = SignUpPageLocators.EMAIL_TXT
 
-class Password_Element(Password_Field_Elements):
+class SignUpPassword_Element(Password_Field_Elements):
     """ This class get the search text from the specified locator """
     forEdit = SignUpPageLocators.PASSWORD_CLK
     forRead = SignUpPageLocators.PASSWORD_TXT
+
+class SignIn_Email_Element(Email_Field_Elements):
+    """ This class get the search text from the specified locator """
+    forEdit = SignInPageLocators.EMAIL_CLK
+    forRead = SignInPageLocators.EMAIL_TXT
+
+class SignInPassword_Element(Password_Field_Elements):
+    """ This class get the search text from the specified locator """
+    forEdit = SignInPageLocators.PASSWORD_CLK
+    forRead = SignInPageLocators.PASSWORD_TXT
+
+class Forgot_Element(Forgot_Password_Field_Elements):
+    """ This class get the search text from the specified locator """
+    forEdit = SignInPageLocators.EMAIL_FORGOT_PASSWORD
 
 class BasePage(object):
     """ Base class to initialize the driver for every test case """
@@ -38,8 +52,8 @@ class HomePage(BasePage):
 
 class SignUpPage(HomePage):
 
-    email_element = Email_Element()
-    pwd_element = Password_Element()
+    email_element = SignUp_Email_Element()
+    password_element = SignUpPassword_Element()
 
     def navigate_to_sign_up_page(self):
         """ Add action items for entering to SignIn Page """
@@ -98,4 +112,44 @@ class ContentPage(HomePage):
         dropdown = self.profile_cirlce()
         dropdown.click()
         element = self.wait.until(EC.element_to_be_clickable(ContentPageLocators.LOGOUT_BUTTON))
+        return element
+
+class SignInPage(SignUpPage):
+    """
+    # Elements to be present in the sign-in page
+    1. Email textbox (Element present)
+    2. Password textbox (Element present)
+    3. Forgot password button
+    4. SignUp button
+    5. email error
+    6. password error
+    """
+    email = SignIn_Email_Element()
+    password = SignInPassword_Element()
+    forgot_email_button = Forgot_Element()
+
+    def navigate_to_sign_in_page(self):
+        """ Add action items for entering to SignIn Page """
+
+        # Entering SignIn Section
+        signInButton = self.wait.until(EC.presence_of_element_located(HomePageLocators.SIGNIN))
+        signInButton.click()
+
+    def sign_in(self, email, password):
+        """ Method to Sign In """
+        self.email = email
+        self.password = password
+        self.click_submit_button()
+
+    def click_forgot_password_button(self):
+        """ Click on Forgot Password """
+        element = self.driver.find_element(*SignInPageLocators.FORGOT_PASSWORD)
+        element.click()
+
+    def password_reactivation(self):
+        """ Methods to send email reactivation link """
+        self.forgot_email_button = SignInPageLocators.USER_EMAIL
+        self.driver.find_element(*SignInPageLocators.SIGNIN).click()
+
+        element = self.driver.find_element(*SignInPageLocators.EMAIL_CONFIRMATION)
         return element
