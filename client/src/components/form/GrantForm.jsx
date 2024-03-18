8315893ac9daa900fa7fc9e085c464a5e3479fda
Shyam Bisht <vinayaksharmaofficial24@gmail.com>
2024-03-18 15:24:42 +0000

    fixed post user req isue

--- client/src/components/form/GrantForm.jsx
+++ client/src/components/form/GrantForm.jsx
@@ -12,7 +12,7 @@ function GrantForm({ closeModal, cardData }) {
     setInputValue(newInputValue);
   };
 
-  const handleSubmit = async(event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
     const requestedAmount = parseFloat(inputValue);
@@ -31,9 +31,10 @@ function GrantForm({ closeModal, cardData }) {
     };
 
     try {
-      // Make the Axios POST request
-      const response = await axios.post(`${process.env.API_BASE_URL}/api/user-requests`, grantData);
-      
+      const api_url = `${process.env.REACT_APP_API_BASE_URL}api/user-requests`;
+      console.log(api_url)
+      const response = await axios.post(api_url, grantData);
+
       console.log('Response from server:', response.data);
       // Handle the response as needed
 
@@ -57,30 +58,30 @@ function GrantForm({ closeModal, cardData }) {
         <h2 className="text-lg mb-4">Grant Application</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
-          <div className="mb-4">
-                        <label htmlFor="userName" className="block text-sm font-medium text-gray-700">
-                            User Name:
-                        </label>
-                        <input
-                            type="text"
-                            id="userName"
-                            value={user.email}
-                            className="w-full px-3 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-green-500 focus:ring-1"
-                            readOnly
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                            Email:
-                        </label>
-                        <input
-                            type="email"
-                            id="email"
-                            value={user.email}
-                            className="w-full px-3 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-green-500 focus:ring-1"
-                            readOnly
-                        />
-                    </div>
+            <div className="mb-4">
+              <label htmlFor="userName" className="block text-sm font-medium text-gray-700">
+                User Name:
+              </label>
+              <input
+                type="text"
+                id="userName"
+                value={user.email}
+                className="w-full px-3 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-green-500 focus:ring-1"
+                readOnly
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                Email:
+              </label>
+              <input
+                type="email"
+                id="email"
+                value={user.email}
+                className="w-full px-3 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-green-500 focus:ring-1"
+                readOnly
+              />
+            </div>
             <label htmlFor="projectDescription" className="block text-sm font-medium text-gray-700">
               Project Description:
             </label>
