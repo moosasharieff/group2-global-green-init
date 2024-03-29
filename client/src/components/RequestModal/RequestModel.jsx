75fdeb22de4aa8c22c3d63082515acc376dc942f
Vinatak Sharma <vinayaksharmaofficial24@gmail.com>
2024-03-29 16:51:46 +0000

    Fixed request model bug issue

--- client/src/components/RequestModal/RequestModel.jsx
+++ client/src/components/RequestModal/RequestModel.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function RequestModel({ isOpen, onClose }) {
+  const { user } = useAuth0();
   const [userRequests, setUserRequests] = useState([]);
   const [grantMessage, setGrantMessage] = useState("");
 
@@ -10,7 +12,7 @@ function RequestModel({ isOpen, onClose }) {
       const API_BASE_URL = `${process.env.REACT_APP_API_BASE_URL}api/get-user-requests`;
       try {
         const response = await axios.get(API_BASE_URL, {
-          params: { email: "bishtshyam136@gmail.com" },
+          params: { email: user.email },
         });
         setUserRequests(response.data);
       } catch (error) {
@@ -37,21 +39,31 @@ function RequestModel({ isOpen, onClose }) {
           &#x2715;
         </button>
         <div className="p-4 max-h-80 overflow-y-auto no-scrollbar">
-          <ul>
-            {userRequests.map((request, index) => (
-              <li
-                className="w-full rounded-lg border p-4 m-2 cursor-pointer hover:bg-slate-500"
-                key={index}
-              >
-                <span className="font-bold">{request.granterName}</span>
-                <span className={request.grantStatus === true ? "text-green-600" : "text-yellow-600"}>
-                  {request.grantStatus === true
-                    ? " Approved"
-                    : " Reviewing your application"}
-                </span>
-              </li>
-            ))}
-          </ul>
+          {userRequests && userRequests.length >= 1 ? (
+            <ul>
+              {userRequests.map((request, index) => (
+                <li
+                  className="w-full rounded-lg border p-4 m-2 cursor-pointer hover:bg-slate-500"
+                  key={index}
+                >
+                  <span className="font-bold">{request.granterName}</span>
+                  <span
+                    className={
+                      request.grantStatus === true
+                        ? "text-green-600"
+                        : "text-yellow-600"
+                    }
+                  >
+                    {request.grantStatus === true
+                      ? " Approved"
+                      : " Reviewing your application"}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <h1>No requests found!!</h1>
+          )}
         </div>
       </div>
     </div>
